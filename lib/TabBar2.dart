0e0d386e3f66c3dbacb1930fe01b3395fe472d4d
Archoooo <andhikameidzakp@gmail.com>
2024-10-10 22:15:32 +0700

    TabBar v.2.1

--- lib/TabBar2.dart
+++ lib/TabBar2.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -32,11 +30,11 @@ class tabBar2 extends StatelessWidget {
         child: Scaffold(
           appBar: AppBar(
             title: Text(
-              "Thick",
+              "wtsup",
               style: TextStyle(
                   fontFamily: 'stick', fontSize: 30, color: Colors.white),
             ),
-            centerTitle: true,
+            centerTitle: false,
             backgroundColor: Color.fromARGB(255, 4, 7, 40),
             bottom: TabBar(
               tabs: mt,
@@ -55,45 +53,287 @@ class tabBar2 extends StatelessWidget {
             children: [
               ListView(
                 children: [
-                  ListTile(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage('images/avt1.png'),
-                    ),
-                    title: Text(
-                      "Udin Bengkel",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                      "we motor mu dah jadi gada ngebon-ngebon",
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    trailing: Text(
-                      "21.35",
-                      textAlign: TextAlign.right,
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(),
+                      title: Text(
+                        "Udin Bengkel",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "we motor mu dah jadi gada ngebon-ngebon",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Text(
+                        "21.35",
+                        textAlign: TextAlign.right,
+                      ),
                     ),
                   ),
+                  Divider(
+                    color: Color.fromARGB(216, 185, 185, 185),
+                  ),
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(),
+                      title: Text(
+                        "rama indosat",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "malas aku cuk",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Text(
+                        "11.35",
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(216, 185, 185, 185),
+                  ),
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(),
+                      title: Text(
+                        "risky kunci 12",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "woila cik",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Text(
+                        "10.00",
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(216, 185, 185, 185),
+                  ),
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(),
+                      title: Text(
+                        "usman goat",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "w baru bangun jir",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Text(
+                        "07.21",
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(216, 185, 185, 185),
+                  ),
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(),
+                      title: Text(
+                        "risa jagorawi",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "nama bapaknya ridwan saha",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Text(
+                        "06.25",
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(216, 185, 185, 185),
+                  ),
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(),
+                      title: Text(
+                        "Maling pangsit",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "woi cuk",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Text(
+                        "06.21",
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(216, 185, 185, 185),
+                  ),
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(),
+                      title: Text(
+                        "ridho simanjuntak",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "gua udah sama si amba jir lu dimana",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Text(
+                        "05.40",
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(216, 185, 185, 185),
+                  ),
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(),
+                      title: Text(
+                        "ambatron",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        "woi katanya mo joging asw",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Text(
+                        "05.35",
+                        textAlign: TextAlign.right,
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Color.fromARGB(216, 185, 185, 185),
+                  )
                 ],
               ),
               ListView(
-                children: const [
-                  ListTile(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
+                children: [
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.black,
+                      ),
+                      title: Text(
+                        "Status Saya",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      subtitle: Text("12.99"),
+                      trailing: TextButton(
+                        onPressed: ColorScheme.dark,
+                        child: Text(
+                          "...",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
-                    title: Text(
-                      "Status Saya",
-                      style: TextStyle(fontSize: 18),
+                  ),
+                  Container(
+                    height: 1,
+                    child: Divider(),
+                  ),
+                  Container(
+                    padding: EdgeInsetsDirectional.symmetric(horizontal: 24),
+                    height: 20,
+                    child: Title(
+                        color: Colors.black, child: Text("update recent")),
+                  ),
+                  Container(
+                    height: 2,
+                    child: Divider(),
+                  ),
+                  Container(
+                    height: 65,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.black,
+                      ),
+                      title: Text(
+                        "someone",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      subtitle: Text("12.99"),
+                      trailing: TextButton(
+                        onPressed: ColorScheme.dark,
+                        child: Text(
+                          "...",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
-                    subtitle: Text("12.99"),
-                    trailing: TextButton(
-                      onPressed: ColorScheme.dark,
-                      child: Text(":"),
+                  ),
+                  Container(
+                    height: 2,
+                    child: Divider(),
+                  ),
+                  Container(
+                    height: 70,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.black,
+                      ),
+                      title: Text(
+                        "Someone",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      subtitle: Text("12.99"),
+                      trailing: TextButton(
+                        onPressed: ColorScheme.dark,
+                        child: Text(
+                          "...",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
+                  ),
+                  Container(
+                    height: 2,
+                    child: Divider(),
                   ),
                   // Divider(
                   //   height: 2,
@@ -106,22 +346,44 @@ class tabBar2 extends StatelessWidget {
               ),
               ListView(
                 children: [
-                  ListTile(
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
-                    ),
-                    title: Text("Create Call Link"),
-                    subtitle: Container(
-                      child: Icon(
-                        Icons.call_made_rounded,
-                        size: 17,
+                  Container(
+                    height: 65,
+                    child: ListTile(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.black,
+                      ),
+                      title: Text("Create Call Link"),
+                      subtitle: Container(
+                        child: Row(
+                          children: [
+                            Icon(Icons.link_rounded), // Tanda panah
+                            SizedBox(width: 10), // Memberi jarak
+                          ],
+                        ),
                       ),
                     ),
                   ),
+                  Container(
+                    height: 1,
+                    child: Divider(),
+                  ),
+                  Container(
+                    padding: EdgeInsetsDirectional.symmetric(horizontal: 24),
+                    height: 20,
+                    child:
+                        Title(color: Colors.black, child: Text("call recent")),
+                  ),
+                  Container(
+                    height: 1,
+                    child: Divider(),
+                  ),
+                  Center(
+                    child: Image.asset("images/LB_call.png"),
+                    heightFactor: 1.4,
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
