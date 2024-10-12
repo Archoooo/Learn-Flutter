0ead23e1c514dac765bbe7a808aabc36d0a661f8
Archoooo <andhikameidzakp@gmail.com>
2024-10-12 21:25:26 +0700

    txtfield v 1.2

--- lib/tXtField.dart
+++ lib/tXtField.dart
@@ -8,33 +8,43 @@ class txtF extends StatelessWidget {
         appBar: AppBar(),
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(10),
+            padding: const EdgeInsets.symmetric(vertical: 20),
             child: Column(
               children: [
-                Text("username"),
                 Container(
                   child: Padding(
                     padding: const EdgeInsets.all(10),
                     child: TextField(
-                      // autocorrect: true,
-                      autofocus: true,
-                      // // obscureText: true,
-                      // // obscuringCharacter: "V",
-                      // keyboardType: TextInputType.datetime,
-                      showCursor: true,
-
-                      decoration: InputDecoration(
+                        // autocorrect: true,
+                        autofocus: true,
+                        // // obscureText: true,
+                        // // obscuringCharacter: "V",
+                        // keyboardType: TextInputType.datetime,
+                        showCursor: true,
+                        decoration: InputDecoration(
                           icon: Icon(
                             Icons.person_outline_sharp,
                             size: 40,
                           ),
-                          border: OutlineInputBorder()),
-                    ),
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 2, 86, 255))),
+                          hintStyle:
+                              TextStyle(color: Colors.grey, fontSize: 12),
+                          hintText: "@user233",
+                          floatingLabelStyle:
+                              TextStyle(color: Color.fromARGB(255, 2, 86, 255)),
+                          labelText: "username",
+                          // labelStyle:
+                          //     TextStyle(color: Colors.blue, fontSize: 20)
+                          // prefixText: "@user122"
+                        )),
                   ),
                 ),
                 Container(
                   height: 20,
-                  child: Text("password"),
+                  // child: Text("password"),
                 ),
                 Container(
                   child: Padding(
@@ -42,7 +52,7 @@ class txtF extends StatelessWidget {
                     child: TextField(
                       // autocorrect: true,
                       // autofocus: true,
-                      // // obscureText: true,
+                      obscureText: true,
                       obscuringCharacter: "*",
                       // keyboardType: TextInputType.datetime,
                       showCursor: true,
@@ -51,7 +61,24 @@ class txtF extends StatelessWidget {
                             Icons.lock_reset,
                             size: 40,
                           ),
-                          border: OutlineInputBorder()),
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 2, 86, 255))),
+                          hintText: "ytta",
+                          hintStyle:
+                              TextStyle(color: Colors.grey, fontSize: 12),
+                          floatingLabelStyle:
+                              TextStyle(color: Color.fromARGB(255, 2, 86, 255)),
+                          labelText: "Password",
+                          suffixIcon: IconButton(
+                              onPressed: () {
+                                //untuk fungsi icon
+                                //harus statefull
+                              },
+                              icon: Icon(Icons.remove_red_eye_rounded))
+                          // prefixText: "password"
+                          ),
                     ),
                   ),
                 ),
